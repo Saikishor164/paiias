0c16e9ded1a865d4687ab7ea19bc4ca36a4115dd
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-08-09 00:15:19 +0530

    updated

--- index.php
+++ index.php
@@ -5,13 +5,21 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+<link rel="stylesheet" href="./stylesheet.css" type="text/css" >
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<style>
+  .img{
+    background: url(./Campus-Staff_1.png);
+    
+  }
+</style>
+
   </head>
   <body>
   <?php include 'header.php'; ?>
-<img src="./Campus-Staff_1.png">
+<img src="./Campus-Staff_1.png" alt="campus staff">
+
 
 <?php include 'footer.php'; ?>
     <!-- Optional JavaScript -->
