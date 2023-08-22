a676bb4f41f3ce1bf74efbef21cb8d24a76cb1c3
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-08-22 20:41:21 +0530

    mades changes in database

--- paiias.sql
+++ paiias.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 10, 2023 at 07:36 AM
+-- Generation Time: Aug 22, 2023 at 05:10 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,6 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `application`
+--
+
+CREATE TABLE `application` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `dob` date NOT NULL,
+  `father` varchar(255) NOT NULL,
+  `local_address` text NOT NULL,
+  `pin1` varchar(10) NOT NULL,
+  `phoneno` varchar(15) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `copy_address` tinyint(4) NOT NULL,
+  `permanent_address` text NOT NULL,
+  `pin2` varchar(10) NOT NULL,
+  `phonenop` varchar(15) DEFAULT NULL,
+  `medium` varchar(50) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `optional_subject` varchar(255) DEFAULT NULL,
+  `ma_msc_mcom_board` varchar(255) DEFAULT NULL,
+  `ma_msc_mcom_year` int(11) DEFAULT NULL,
+  `ma_msc_mcom_subjects` varchar(255) DEFAULT NULL,
+  `ma_msc_mcom_percentage` decimal(5,2) DEFAULT NULL,
+  `ba_bsc_bcom_eng_board` varchar(255) DEFAULT NULL,
+  `ba_bsc_bcom_eng_year` int(11) DEFAULT NULL,
+  `ba_bsc_bcom_eng_subjects` varchar(255) DEFAULT NULL,
+  `ba_bsc_bcom_eng_percentage` decimal(5,2) DEFAULT NULL,
+  `intermediate_board` varchar(255) DEFAULT NULL,
+  `intermediate_year` int(11) DEFAULT NULL,
+  `intermediate_subjects` varchar(255) DEFAULT NULL,
+  `intermediate_percentage` decimal(5,2) DEFAULT NULL,
+  `matric_board` varchar(255) DEFAULT NULL,
+  `matric_year` int(11) DEFAULT NULL,
+  `matric_subjects` varchar(255) DEFAULT NULL,
+  `matric_percentage` decimal(5,2) DEFAULT NULL,
+  `others_board` varchar(255) DEFAULT NULL,
+  `others_year` int(11) DEFAULT NULL,
+  `others_subjects` varchar(255) DEFAULT NULL,
+  `others_percentage` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -35,15 +81,15 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
-(1, 'admin', '$2y$10$jdzXlR0sVaGZxMDAYXO64OWIF8NRrtb.qV7EtaQNf3nLtIRWAB13a', '2023-08-10 11:05:39');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `application`
+--
+ALTER TABLE `application`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_application` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -57,10 +103,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `application`
+--
+ALTER TABLE `application`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `application`
+--
+ALTER TABLE `application`
+  ADD CONSTRAINT `fk_user_application` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
