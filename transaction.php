f76e0eb154802cf0666893ed22284748564f4021
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-08-23 16:13:55 +0530

    updated welcome.php and transcation.php

--- transaction.php
+++ transaction.php
@@ -1,5 +1,30 @@
-<?php include 'header.php' ; ?>
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+  header("location: login.php");
+  exit;
+}
+
+require 'config.php';
 
 
+include 'logheader.php' ;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction</title>
+</head>
+<body>
+    
+</body>
+</html>
 
 <?php include 'footer.php' ;?>
