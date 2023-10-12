9fddf3c666b4957aec08252c9d0aff8293123543
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-10-12 09:52:24 +0530

    added footer to the website

--- footer.php
+++ footer.php
@@ -1,33 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="stylesheet.css" type="text/css">
-</head>
-<body>
-<footer class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h3>About Us</h3>
-                <p>Your description about the website or company.</p>
-            </div>
-            <div class="col-md-6">
-                <h3>Contact Us</h3>
-                <p>
-                    <br> </p>
-            </div>
-        </div>
-        <hr>
-        <p class="text-center">&copy; 2023 Your Website. All rights reserved.</p>
-    </div>
-</footer>
-</body>
-</html> -->
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -48,17 +18,20 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <h3>About Us</h3>
-                        <p>Your description about the website or company.</p>
+                        <h3>Address</h3>
+                        <p>P A INAMDAR IAS COACHING INSTITUTE 
+                <br> 2390-B, K.B, Hidayatullah Road, Azam Campus, 
+                Pune Cantonment, Camp, Pune, Maharashtra - 411001
+</p>
                     </div>
                     <div class="col-md-6">
                         <h3>Contact Us</h3>
-                        <p>Email: contact@example.com<br>
-                        Phone: (123) 456-7890</p>
+                        <p>Email: pai-ias@azamcampus.org<br>
+                        </p>
                     </div>
                 </div>
                 <hr>
-                <p class="text-center">&copy; 2023 Your Website. All rights reserved.</p>
+                <p class="text-center h6">Copyright &copy; 2023 | P. A. Inamdar IAS Coaching Institute, Pune | Devloped by AZAM I.T</p>
             </div>
         </footer>
     <!-- </div> -->
