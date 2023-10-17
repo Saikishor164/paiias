8f76616b0562b553e8f5aeb43a16ee7d3028e197
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-10-17 14:01:16 +0530

    updated footer.php

--- footer.php
+++ footer.php
@@ -32,7 +32,13 @@
                 </div>
                 <hr>
 
+
                 
+
+
+
+
+
                 <p class="text-center h6">Copyright &copy; 2023 | P. A. Inamdar IAS Coaching Institute, Pune | Devloped by AZAM I.T</p>
             </div>
         </footer>
