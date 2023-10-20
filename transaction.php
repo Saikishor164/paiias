82e1027a73b1eecd432cced5cfb160b2e8a0d2e2
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-10-20 09:57:28 +0530

    updated some changes

--- transaction.php
+++ transaction.php
@@ -18,12 +18,43 @@ include 'logheader.php' ;
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheet.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- Required meta tags -->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <!-- <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <title>Transaction</title>
 </head>
 <body>
+
+<div class="h1">Saikishor</div>
+<div class="h1">Saikshor</div>
+<div class="h1">Saikishor</div>
     
+<img src="saikishor164.github.io/saikishor164/sample.png" alt="Sample Images " srcset="">
+
+
+<div class="container">Hello world </div>
+
+<div class="wrapper">Hello world</div>
+
+<div class="div">saikishor Rasala</div>
+
+<div class="school">Hello <i class="fas fa-wordpress-simple    "></i></div>
+
+<i></i>
+
+
+
+
 </body>
 </html>
 
