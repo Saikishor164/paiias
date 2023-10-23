d05c0a1efb1918bad2955ea5a89f2cee7b495d45
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-10-23 16:22:24 +0530

    uploaded images and created Gallery page.

---
+++ gallery.php
@@ -0,0 +1,57 @@
+<?php include 'header.php'; ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PAI-IAS Gallery</title>
+</head>
+<body>
+<div class="container">
+<div class="row">
+  <div class="col-md-6">
+    <div class="thumbnail">
+        <img src="./images/1.jpg" alt="Lights" style="width:100%">
+        <div class="caption">
+          <!--<p class="text-white text-center h3">ClassRoom</p>-->
+          <br><br>
+        </div>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="thumbnail">
+        <img src="./images/2.jpg" alt="Nature" style="width:100%">
+        <div class="caption">
+          <!--<p>Lorem ipsum...</p><br><br>-->
+        </div>
+    </div>
+  </div>
+  
+
+  <div class="col-md-6">
+    <div class="thumbnail">
+        <img src="./images/3.jpg" alt="Lights" style="width:100%">
+        <div class="caption">
+          <!--<p>Lorem ipsum...</p> <br><br>-->
+        </div>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="thumbnail">
+        <img src="./images/5.jpg" alt="Nature" style="width:100%">
+        <div class="caption">
+          <!--<p>Lorem ipsum...</p><br><br>-->
+        </div>
+    </div>
+  </div>
+  <br><br><br>
+
+</div>
+</div>
+</body>
+</html>
+
+<br><br>br>
+<?php include 'footer.php'; ?>
